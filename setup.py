037d362ab6816681836ea56461887c4d96a4e128
potfur <wachowski.michal@gmail.com>
2017-06-24 09:51:42 +0200

    project setup

---
+++ setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='pollen',
+    packages=find_packages(exclude=[]),
+    version='0.1.0',
+    description='Dependency injection container.',
+    long_description=readme,
+    author='Michal Wachowski',
+    author_email='wachowski.michal@gmail.com',
+    url='https://github.com/potfur/pollen',
+    keywords=[
+        'dependency injection',
+        'container',
+        'inversion of control',
+    ],
+    test_suite='tests',
+    tests_require=[
+        'pytest',
+        'flake8'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
